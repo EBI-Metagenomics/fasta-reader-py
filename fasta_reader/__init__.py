5c49d29f7e01c439d9cbc5a377fc41d6b51aadc4
Danilo Horta <danilo.horta@pm.me>
2019-11-07 16:23:40 +0000

    export Item

--- fasta_reader/__init__.py
+++ fasta_reader/__init__.py
@@ -1,6 +1,6 @@
-from ._reader import open_fasta, FASTAParser, ParsingError
+from ._reader import FASTAParser, Item, ParsingError, open_fasta
 from ._testit import test
 
 __version__ = "0.0.1"
 
-__all__ = ["__version__", "test", "open_fasta", "FASTAParser", "ParsingError"]
+__all__ = ["__version__", "test", "open_fasta", "FASTAParser", "ParsingError", "Item"]
