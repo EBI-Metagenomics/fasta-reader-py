efa16ffa40e25aa4e6b7fa7b247223e0f5235392
Danilo Horta <danilo.horta@pm.me>
2019-12-02 20:38:45 +0000

    0.0.2

--- fasta_reader/__init__.py
+++ fasta_reader/__init__.py
@@ -1,6 +1,6 @@
 from ._reader import FASTAParser, Item, ParsingError, open_fasta
 from ._testit import test
 
-__version__ = "0.0.1"
+__version__ = "0.0.2"
 
 __all__ = ["__version__", "test", "open_fasta", "FASTAParser", "ParsingError", "Item"]
