fb47e7e244a4d5059bc8befff80e43199393b4dd
Danilo Horta <danilo.horta@pm.me>
2020-01-09 16:19:34 +0000

    fasta writer

--- fasta_reader/__init__.py
+++ fasta_reader/__init__.py
@@ -1,6 +1,15 @@
-from ._reader import FASTAParser, Item, ParsingError, open_fasta
+from ._parser import FASTAParser, Item, ParsingError, open_fasta
+from ._writer import FASTAWriter
 from ._testit import test
 
 __version__ = "0.0.2"
 
-__all__ = ["__version__", "test", "open_fasta", "FASTAParser", "ParsingError", "Item"]
+__all__ = [
+    "__version__",
+    "test",
+    "open_fasta",
+    "FASTAParser",
+    "ParsingError",
+    "Item",
+    "FASTAWriter",
+]
---
+++ fasta_reader/_writer.py
@@ -0,0 +1,35 @@
+import pathlib
+from typing import IO, Union
+
+
+class FASTAWriter:
+    def __init__(self, file: Union[str, pathlib.Path, IO[str]]):
+        if isinstance(file, str):
+            file = pathlib.Path(file)
+
+        if isinstance(file, pathlib.Path):
+            file = open(file, "r")
+
+        self._file = file
+
+    def write_item(self, defline: str, sequence: str):
+        """
+        Write item.
+        """
+        self._file.write(">" + defline + "\n")
+        self._file.write(sequence + "\n")
+
+    def close(self):
+        """
+        Close the associated stream.
+        """
+        self._file.close()
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exception_type, exception_value, traceback):
+        del exception_type
+        del exception_value
+        del traceback
+        self.close()
