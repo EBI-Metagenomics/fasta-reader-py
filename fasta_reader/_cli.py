e02666ea4ba126643ed5a3f0daeede419a888169
Danilo Horta <danilo.horta@pm.me>
2020-12-04 13:05:40 +0000

    fasta-show ncols

--- fasta_reader/_cli.py
+++ fasta_reader/_cli.py
@@ -1,18 +1,22 @@
+import sys
 from statistics import mean
 
 import click
 
 from ._reader import read_fasta
 from ._version import __version__
+from ._writer import write_fasta
 
 
 @click.command(context_settings=dict(help_option_names=["-h", "--help"]))
 @click.version_option(__version__)
 @click.argument("fasta", type=click.Path(exists=True))
+@click.option("--stats/--no-stats", default=True, help="Show sequence statistics.")
 @click.option(
     "--hist/--no-hist", default=False, help="Show histogram of sequence lengths."
 )
-def cli(fasta, hist: bool):
+@click.option("--ncols", default=0, help="Show formatted sequences.")
+def cli(fasta, stats: bool, hist: bool, ncols: int):
     """
     Show information about FASTA files.
 
@@ -31,14 +35,20 @@ def cli(fasta, hist: bool):
         abc |= set(item.sequence)
         nitems += 1
 
-    usymbols = "".join(sorted(list(abc)))
-    click.echo(f"Number of sequences: {nitems}")
-    click.echo(f"Unique sequence symbols: {usymbols}")
-    msg = f"Sequence length: min {min(seq_lens)}, mean {mean(seq_lens)}, max {max(seq_lens)}"
-    click.echo(msg)
+    if stats:
+        usymbols = "".join(sorted(list(abc)))
+        click.echo(f"Number of sequences: {nitems}")
+        click.echo(f"Unique sequence symbols: {usymbols}")
+        msg = f"Sequence length: min {min(seq_lens)}, mean {mean(seq_lens)}, max {max(seq_lens)}"
+        click.echo(msg)
 
     if hist:
         show_hist(seq_lens)
+
+    if ncols > 0:
+        with write_fasta(sys.stdout, ncols) as writer:
+            for item in read_fasta(fasta):
+                writer.write_item(item.defline, item.sequence)
 
 
 def show_hist(seq_lengths):
